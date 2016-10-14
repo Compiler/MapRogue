5968d4c63180e5e94ec4fc9f0138ba99be6bfa0f
Luke <lr98lr@aol.com>
2016-10-14 15:52:12 -0400

    Perfectly decodes and fills map in all ways

--- RogueLike/Decoder.h
+++ RogueLike/Decoder.h
@@ -2,6 +2,7 @@
 #include "Sector.h"
 #include "string"
 #include "iostream"
+#include "StringEncode.h"
 #include "vector"
 
 class Decoder{
@@ -11,14 +12,29 @@ public:
 	static void decode(std::vector<Sector>& sectors, std::string& stream){
 	
 		std::cout << "Original stream:\n" << stream << std::endl;
+		std::vector<StringEncode> pieces;
+		StringEncode tmpS;
 		for(int i = 0; i < sectors.size(); i++){
+
 			std::string tmp = stream.substr(sectors[i].beginning, sectors[i].ending - sectors[i].beginning);
 		    std::size_t pos = tmp.find_last_not_of("0123456789");
-			std::cout << "Print " << tmp.substr(0, pos) << " of " << tmp[pos] << std::endl;
-		
+
+			int amount = std::stoi(tmp.substr(0, pos));
+			std::cout << "Print " << amount << " of " << tmp[pos] << std::endl;
+			tmpS.amount = amount;
+			tmpS.key = tmp[pos];
+			pieces.push_back(tmpS);
+
 		}
 
+		std::string finalString = "";
+		for(int i = 0; i < pieces.size(); i++)
+			for(int k = 0; k < pieces[i].amount; k++)
+				finalString += pieces[i].key;
+
 	//	bool tra = !stream.empty() && stream.find_first_not_of("0123456789") == std::string::npos;
+
+		stream = finalString;
 	}
 
 
---
+++ RogueLike/StringEncode.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+struct StringEncode{
+
+	int amount;
+	char key;
+
+};
