ef6a9ca2270ce1e315402e82940dd087e8aa83ea
Luke <lr98lr@aol.com>
2016-10-14 15:41:28 -0400

    Picking out strings

--- RogueLike/Decoder.h
+++ RogueLike/Decoder.h
@@ -13,12 +13,12 @@ public:
 		std::cout << "Original stream:\n" << stream << std::endl;
 		for(int i = 0; i < sectors.size(); i++){
 			std::string tmp = stream.substr(sectors[i].beginning, sectors[i].ending - sectors[i].beginning);
-			std::cout << sectors[i].beginning << " to " << sectors[i].ending << " is " << tmp << "\n" << std::endl;
+		    std::size_t pos = tmp.find_last_not_of("0123456789");
+			std::cout << "Print " << tmp.substr(0, pos) << " of " << tmp[pos] << std::endl;
 		
 		}
 
 	//	bool tra = !stream.empty() && stream.find_first_not_of("0123456789") == std::string::npos;
-		std::size_t pos = stream.find_last_not_of("0123456789");
 	}
 
 
